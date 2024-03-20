f85a6743c24fdf289f3876cd627ddf26dd74570d
Caue Sordi Paulino <132071046+caueSordi@users.noreply.github.com>
2024-03-20 11:24:42 -0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -25,8 +25,43 @@ if response.status_code == 200:
     description = data['weather'][0]['description']  # picks the description from the raw data, 'weather is the group [0] is the position of the wanted info and  'descriptiom' is the wanetd info
     print(f"location: {city_name}")
     print(f"temperature: {temperature:.2f}°C")
+    if temperature > 30:
+        print("It's hot today")
+    elif temperature < 20 > 10:
+        print("It's cold today")
+    elif temperature <10:
+        print("you should wear a coat today")
+    else:
+        print("the weather is nice today :)")
+
     print(f"timezone: {time_zone}")
     print(f"Today there will be: {description}")
+    if description == 'clear sky':
+        print("Remember to use sun screen")
+    elif description == 'rain':
+        print("you will need an umbrella")
+    elif description == 'scattered clouds':
+        print("I recomend you to take an umbrella")
+
+    if temperature > 20 and description == 'clear sky' or description == 'scattered clouds' :
+        print("The weather is perfect for a picnic")
+    elif temperature <15 and description == 'rain' or description == 'overcast clouds'or description == 'broken clouds':
+        print("such a cozy day to watch a film right?")
+        answer = input("do you have a partner to watch it with?\n")
+        
+        if answer == 'yes':
+            print("you are lucky")
+        elif answer == 'no':
+            print("you should call someone \n")
+        else:
+            print("I don't know what to say about that")
+            lonely = input("do you have someone to call? \n")
+            if lonely == 'yes':
+                print("then you problably should call them")
+            else:
+                print("it's okay to be alone sometimes, but there's a whole of people out there waiting for you to meet them")
+
+        
     print('have a nice day :)')
 else:
     print("Failed ", response.status_code)
